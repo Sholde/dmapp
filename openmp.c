c47c5ecfe475f2a5d731797dffb6ee1f62747f45
Sholde <exshode@gmail.com>
2021-04-29 23:15:28 +0200

    adding openmp code

--- openmp.c
+++ openmp.c
@@ -1,55 +1,78 @@
-#include <omp.h>
 #include <stdio.h>
 #include <assert.h>
+#include <omp.h>
 
-/*Paralléliser avec OpenMP le calcul de la somme suivante*/
+/*
+ * Paralléliser avec OpenMP le calcul de la somme suivante
+ */
+
 #define max_iter_exo1 1000
-void exo1(){
+
+void exo1(void)
+{
+  //
   int i;
-  int somme = 0;
-  for(i = 0; i < max_iter_exo1; i++){
-    somme += i; 
-  }
+  int sum = 0;
+
+  // Create OMP_NUM_THREADS and make a reduction on variable sum
+#pragma omp parallel for default(none) private(i) reduction(+:sum)
+  for(i = 0; i < max_iter_exo1; i++)
+    {
+      sum += i; 
+    }
 
   /*Vérification du résultat*/
-  fprintf(stderr,"Somme %d attendu %d\n",somme, ((max_iter_exo1-1)*max_iter_exo1)/2);
-  assert(somme == ((max_iter_exo1-1)*max_iter_exo1)/2);
+  fprintf(stderr, "Sum %d expected %d\n", sum, ((max_iter_exo1 - 1) * max_iter_exo1 ) / 2);
+  assert(sum == ((max_iter_exo1 - 1) * max_iter_exo1) / 2);
 }
 
 
 /*
-  Expliquer les valeurs des adresses de chacune des variables suivantes: a,b,c,d. Expliquer pourquoi elle sont différentes ou identiques.
-*/
+ * Expliquer les valeurs des adresses de chacune des variables suivantes: a,b,c,d.
+ * Expliquer pourquoi elle sont différentes ou identiques.
+ */
+
 int a;
 int b;
 #pragma threadprivate(b)
-void exo2(){
+
+void exo2(void)
+{
+  //
   static int c;
   int d;
   int e;
-  int f; 
+  int f;
+
+  // Print in omp
 #pragma omp parallel private(e) shared(f)
   {
-    fprintf(stderr,"\tThread %d a %p\n",omp_get_thread_num(),&a);
-    fprintf(stderr,"\tThread %d b %p\n",omp_get_thread_num(),&b);
-    fprintf(stderr,"\tThread %d c %p\n",omp_get_thread_num(),&c);
-    fprintf(stderr,"\tThread %d d %p\n",omp_get_thread_num(),&d);
-    fprintf(stderr,"\tThread %d e %p\n",omp_get_thread_num(),&e);
-    fprintf(stderr,"\tThread %d f %p\n",omp_get_thread_num(),&f);
+    fprintf(stderr, "\tThread %d a %p\n", omp_get_thread_num(), &a);
+    fprintf(stderr, "\tThread %d b %p\n", omp_get_thread_num(), &b);
+    fprintf(stderr, "\tThread %d c %p\n", omp_get_thread_num(), &c);
+    fprintf(stderr, "\tThread %d d %p\n", omp_get_thread_num(), &d);
+    fprintf(stderr, "\tThread %d e %p\n", omp_get_thread_num(), &e);
+    fprintf(stderr, "\tThread %d f %p\n", omp_get_thread_num(), &f);
   }
-    fprintf(stderr,"Thread %d a %p\n",omp_get_thread_num(),&a);
-    fprintf(stderr,"Thread %d b %p\n",omp_get_thread_num(),&b);
-    fprintf(stderr,"Thread %d c %p\n",omp_get_thread_num(),&c);
-    fprintf(stderr,"Thread %d d %p\n",omp_get_thread_num(),&d);
-    fprintf(stderr,"Thread %d e %p\n",omp_get_thread_num(),&e);
-    fprintf(stderr,"Thread %d f %p\n",omp_get_thread_num(),&f);
+
+  // Print out omp
+  fprintf(stderr, "Thread %d a %p\n", omp_get_thread_num(), &a);
+  fprintf(stderr, "Thread %d b %p\n", omp_get_thread_num(), &b);
+  fprintf(stderr, "Thread %d c %p\n", omp_get_thread_num(), &c);
+  fprintf(stderr, "Thread %d d %p\n", omp_get_thread_num(), &d);
+  fprintf(stderr, "Thread %d e %p\n", omp_get_thread_num(), &e);
+  fprintf(stderr, "Thread %d f %p\n", omp_get_thread_num(), &f);
 }
 
-int main(int argc, char** argv){
-  fprintf(stderr,"OpenMP\n");
-  
+int main(int argc, char **argv)
+{
+  // Print name of bench
+  fprintf(stderr, "Bench name: OpenMP\n");
+
+  // Run exo
   exo1();
   exo2();
-  
+
+  // Exit
   return 0; 
 }
